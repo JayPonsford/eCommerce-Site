26c3d42d4adfe03568a701de95434ba3120fb699
JayPonsford <jayponsford@hotmail.com>
2023-10-03 12:19:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import {useEffect, useState } from 'react'
+import './app.css'
+import ProductData from './products'
+import { FaHeart, FaChartBar, FaShoppingCart, FaShuttleVan } from 'react-icons/fa';
+import moment from "moment";
+
+const App = () => {
+  
+  const [countdownString, setCountdownString] = useState("");
+  
+  useEffect(() => {
+      const formatDate = () => {
+        const formattedCountdown = moment().endOf('day').diff(moment());
+        const countDown = moment.utc(formattedCountdown).format("hh:mm:ss")
+        setCountdownString(countDown)
+      };
+      const formatDateSecond = setInterval(formatDate, 1000);
+      return () => {
+        clearInterval(formatDateSecond);
+      };
+    }, []);
+
+
+  return (
+    <>
+      {ProductData.map((productData) => (
+        <div key={productData.id} className="flex-container">
+          <div className="flex-item-left">
+          <div className="eRecommend" style={{visibility: productData.isRecommended ? '' : 'hidden'}}>Eclipse Recommended</div>
+              <img className="productImage" src={productData.img} alt={productData.productName}></img>
+              <img className="subLogo" src={productData.imgBrand} alt={productData.productName}></img>
+          </div>
+          <div className="flex-item-centre">
+              <h5 className='productTitle'>{productData.productName}</h5>
+              <div className="Stars" style={{ '--rating': `${productData.stockPercent}` }}><span className="reviewAmount">(11 reviews)</span></div>
+              {productData.productFeatures && (
+                <ul className='productFeatures'>
+                  {productData.productFeatures.map((feature, index) => (
+                    <li key={index}>{feature}</li>
+                  ))}
+                </ul>
+              )}
+              <div className="buttonResponses">
+                <button type="submit">
+                    <FaHeart/>
+                </button>
+                <button type="submit" >
+                    <FaChartBar />
+                </button>
+            </div>
+          </div>
+          <div className="flex-item-right">
+            <div className="pricing">
+                <div className="productPriceDiscount">RRP £{productData.originalPrice}</div>
+                <div className="productPrice">£{productData.discountPrice}</div>
+                <div className="productSavings">Save £200</div>
+                <div className="stock-bar">
+                <div className="stock-level" style={{ width: `${productData.stockPercent}%`, backgroundColor: productData.stockPercent <= 30 ? 'red' : productData.stockPercent <= 55 ? 'yellow' : 'green' }}>                  
+                </div>
+              </div>
+              <div className="stock-text" style={{color: productData.stockPercent <= 30 ? 'red' : productData.stockPercent <= 55 ? 'yellow' : 'green'}}>
+                {productData.stockPercent <= 30 ? 'Low Stock' : productData.stockPercent <= 55 ? 'In Demand' : 'In Stock'}
+              </div>
+            </div>
+            <div className="productDelivery">
+                <span className='deliveryTime'><FaShuttleVan />Order in the next <b>{countdownString}</b> for delivery on <b>{moment().add(1,'days').format("Do MMMM")}</b> </span>
+                <li>FREE UK delivery</li>
+                <li>PayPal credit available</li>
+            </div>
+            <button className="basketButton"><i className="fa-solid fa-cart-shopping"><FaShoppingCart/></i>ADD TO BASKET</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+
+export default App;
---
+++ src/products.js
@@ -0,0 +1,37 @@
+const ProductData = [
+  {
+      id: 1,
+      img: '/LGOLED65-transparent.png',
+      imgBrand: '/LGLogo.png',
+      productName: 'LG OLED65BX6LB 65 Smart TV - A Energy Rated',
+      productFeatures: ['Self-lit pixels',
+      'a7 Gen3 Intelligent Processor 4K',
+      'Transform your TV into a central hub with ThinQ AI',
+      'Dolby Vision IQ & Dolby Atmos',
+      '2 year warranty'],
+      originalPrice: '1,799.00',
+      discountPrice: '1,599.00',
+      stockPercent: 75,
+      starRating: 5,
+      isRecommended: true,
+  },
+  {
+    id: 2,
+    img: '/LGOLED65-transparent.png',
+    imgBrand: '/LGLogo.png',
+    productName: 'LG OLED65BX6LB 65 Smart TV - A Energy Rated',
+    productFeatures: ['Self-lit pixels',
+    'a7 Gen3 Intelligent Processor 4K',
+    'Transform your TV into a central hub with ThinQ AI',
+    'Dolby Vision IQ & Dolby Atmos',
+    '2 year warranty'],
+    originalPrice: '1,799.00',
+    discountPrice: '1,599.00',
+    stockPercent: 25,
+    starRating: 3.5,
+    isRecommended: false,
+
+  }
+]
+
+export default ProductData
